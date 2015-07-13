8b6e14ffee56948e17266fbad9832afa73229eb8
Charles <whitfield.cw@gmail.com>
2015-07-13 09:50:44 -0700

    add ARGV options to runner :

--- app/runner.rb
+++ app/runner.rb
@@ -1,14 +1,38 @@
 require_relative "seed"
 
-# puts "No siblings:"
-# @fam.find_only_children
+if ARGV.length == 0
+  puts "No siblings:"
+  @fam.find_only_children
 
-# puts "Grandparent of Robert:"
-# @fam.find_grandparent("Robert")
+  puts "Grandparent of Kevin:"
+  @fam.find_grandparent("Kevin")
 
-# puts "No children:"
-# @fam.find_non_parents
+  puts "No children:"
+  @fam.find_non_parents
 
-winner = @fam.fpg_recursive(@fam.ancestor)
+  puts "Proudest Grandparent:"
+  @fam.find_proudest_grandparent
+else
+  case (ARGV[0]).downcase
+  when "lonely"
+    puts "No siblings:"
+    @fam.find_only_children
+  when "grandparent"
+    grandkid = ARGV[1]
+    puts "Grandparent of " + grandkid + ":"
+    @fam.find_grandparent(grandkid)
+  when "childless"
+    puts "No children:"
+    @fam.find_non_parents
+  when "proudest"
+    puts "Proudest grandparent:"
+    @fam.find_proudest_grandparent
+  else
+    puts "Couldn't read command."
+  end
 
-puts winner[1].name
+end
+
+# puts winner[1].name
+
+# p ARGV
